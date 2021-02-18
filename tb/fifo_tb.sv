269faf45a83b6b6fe79fc12b70301aed3b8833c2
MorgalNikita <78976288+MorgalNikita@users.noreply.github.com>
2021-02-19 00:33:22 +0300

    Update fifo_tb.sv

--- tb/fifo_tb.sv
+++ tb/fifo_tb.sv
@@ -1,74 +1,79 @@
 `timescale 1ps / 1ps
-module fifo_tb();
+module fifo_tb
+#(
 
-//inp
-	logic 		clk_i;
-	logic 		rst_i;
-	logic 		read;
-	logic 		write;
-	logic [7:0] data_in;
-//outp
-	logic 		fifo_empty;
-	logic 		fifo_full;
-	logic [7:0] data_out;
-	integer 		i;
+	parameter WIDTH_DATA   = 8
+
+);
+
+//input
+	logic 						clk_i;
+	logic 						rst_i;
+	logic 						read_i;
+	logic 						write_i;
+	logic [WIDTH_DATA -1:0] 			data_in;
+//output
+	logic 						fifo_empty_o;
+	logic 						fifo_full_o;
+	logic [WIDTH_DATA-1:0] 				data_out;
+	integer 					i;
 
 	fifo_junior fifo_ut (
-		.clk_i(clk_i), 
-		.rst_i(rst_i), 
-		.write(write), 
-		.read(read), 
-		.fifo_empty(fifo_empty),
-		.fifo_full(fifo_full),
-		.data_in(data_in), 
+		.clk_i(clk_i),
+		.rst_i(rst_i),
+		.write(write_i),
+		.read(read_i),
+		.fifo_empty(fifo_empty_o),
+		.fifo_full(fifo_full_o),
+		.data_in(data_in),
 		.data_out(data_out)
 	);
 always
 	begin
-	clk_i 	= 1;
+	clk_i	= 1;
 	#5;
-	clk_i		= 0;
+	clk_i	= 0;
 	#5;
 	end
 	
-	 task fifo_proc;  
-      begin  
-           for (i = 0; i < 50; i = i + 1) begin: WRE  
-                #(6*5) 
-                write = 1;  
-                data_in = data_in + 1;  
-                #(6)  
-                write = 0;  
-           end  
-           #(6)  
-           for (i = 0; i < 50; i = i + 1) begin: RDE  
-                #(6)  
-                read = 1;  
-                #(6)  
-                read = 0;  
-           end  
-			  #(6)
-			    for (i = 0; i < 50; i = i + 1) begin: RDE_AND_WRE
-                #(6)  
-                read = 	1;
-					 write = 1;
-					 data_in = data_in + 1;  
-                #(6)  
-                read =	0; 
-					 write = 0; 
-           end  
-      end  
-		endtask
+	task fifo_proc;
+		begin
+			for (i = 0; i < 50; i = i + 1) begin: write
+				#(6*5)
+				write_i = 1;
+				data_in = data_in + 1;
+				#(6)
+				write_i = 0;
+			end
+				#(6)
+			for (i = 0; i < 50; i = i + 1) begin: read
+				#(6)
+				read_i = 1;
+				#(6)
+				read_i = 0;
+			end
+				#(6)
+			for (i = 0; i < 50; i = i + 1) begin: read_and_write
+				#(6)
+				read_i 		=	1;
+				write_i 	=	1;
+				data_in 	= 	data_in + 1;
+				#(6)
+				read_i	=	0;
+				write_i	=	0;
+			end
+      end
+	endtask
 		
 initial begin
-	data_in = 2;
-	rst_i	  = 0;
+	data_in			= $urandom(2**WIDTH_DATA);
+	rst_i			= 0;
 	#6;
-	rst_i   = 1;
+	rst_i			= 1;
 	#6;
-	rst_i	  = 0;
+	rst_i			= 0;
 	fifo_proc;
+	$display("Time = %t",$realtime);
 end
-
 
 endmodule
